49b0031f7c6e77bd270f08c6f9465e07073fad2f
Kijin Sung <kijin.sung@gmail.com>
2012-07-27 16:29:16 +0900

    Add copyright and version notice to every class

--- php5.2/file.php
+++ php5.2/file.php
@@ -1,5 +1,37 @@
 <?php
 
+/**
+ * -----------------------------------------------------------------------------
+ *              C O M M O N  L I B R A R I E S  ( P H P  5 . 2 )
+ * -----------------------------------------------------------------------------
+ * 
+ * @package    Common
+ * @author     Kijin Sung <kijin@kijinsung.com>
+ * @copyright  (c) 2012, Kijin Sung <kijin@kijinsung.com>
+ * @license    GPL v3 <http://www.opensource.org/licenses/gpl-3.0.html>
+ * @link       http://github.com/kijin/common
+ * @version    201207.1
+ * 
+ * -----------------------------------------------------------------------------
+ * 
+ * Copyright (c) 2012, Kijin Sung <kijin@kijinsung.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * -----------------------------------------------------------------------------
+ */
+
 class File
 {
     // Some instance variables.
